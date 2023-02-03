0b8bd9ddbf2417183af60227e6c3c06a9a945682
Daniel Clough <115506656+daniellee862@users.noreply.github.com>
2023-02-03 16:26:10 +0000

    not sure

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  cloud {
+    organization = "northcoders-daniel"
+
+    workspaces {
+      name = "lambda-terraform-gh-actions"
+    }
+  }
+}
+
+
 provider "aws" {
     region = "us-east-1"
 }
