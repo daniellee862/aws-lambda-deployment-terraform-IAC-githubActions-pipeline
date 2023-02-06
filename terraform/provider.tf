8e8fe60e9e56c1377dd927d04b91fc19249dbec8
Daniel Clough <115506656+daniellee862@users.noreply.github.com>
2023-02-06 23:27:49 +0000

    keys in provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,6 +8,9 @@ terraform {
   }
 }
 
+
 provider "aws" {
-region = "us-east-1"
+    region = "us-east-1"
+    access_key = "AKIASNH52EVFTTK4CCXS"
+    secret_key = "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"
 }
