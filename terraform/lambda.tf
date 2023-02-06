a866c53ab8bffc0948b5e429ed06d90f08a62580
Daniel Clough <115506656+daniellee862@users.noreply.github.com>
2023-02-06 23:47:05 +0000

    comment out lamda permission

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -7,10 +7,10 @@ handler = "reader.lambda_handler"
 runtime = "python3.9"
 }
 
-resource "aws_lambda_permission" "allow_s3" {
-  action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.s3_file_reader.function_name
-  principal = "s3.amazonaws.com"
-  source_arn = aws_s3_bucket.data_bucket.arn
-  source_account = data.aws_caller_identity.current.account_id
-}
+#resource "aws_lambda_permission" "allow_s3" {
+#  action = "lambda:InvokeFunction"
+#  function_name = aws_lambda_function.s3_file_reader.function_name
+#  principal = "s3.amazonaws.com"
+#  source_arn = aws_s3_bucket.data_bucket.arn
+#  source_account = data.aws_caller_identity.current.account_id
+#}
